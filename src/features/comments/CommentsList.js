1212d12dd83a0cf99bb08e04cffe61d8a4d3d609
Peter Cooper <cooper.peterv@yahoo.com>
2023-11-03 02:29:00 -0500

    "Redux for All"

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { Col } from 'reactstrap';
 // import Comment from './Comment';
 import AnimatedComment from './AnimatedComment';
@@ -5,7 +6,7 @@ import CommentForm from './CommentForm';
 import { selectCommentsByCampsiteId } from './commentsSlice';
 
 const CommentsList = ({ campsiteId }) => {
-    const comments = selectCommentsByCampsiteId(campsiteId);
+    const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
 
     if (comments && comments.length > 0) {
         return (
